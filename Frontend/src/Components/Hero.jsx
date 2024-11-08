688583864d71aa3ccfa62ab0342cf0862bc0d5d2
Yash Rajput <yashdefying@gmail.com>
2024-11-08 18:20:07 +0530

    minor changes

--- Frontend/src/Components/Hero.jsx
+++ Frontend/src/Components/Hero.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export const Hero = () => {
   return (
+    <>
     <div>Welcome</div>
+   <div> <a href="/login">Login</a></div>
+    <div><a href="/signup">signup</a></div>
+    </>
   )
 }
