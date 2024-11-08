fc50f7d283a34dcd655fb3b624d4f4cb84153f42
Yash Rajput <yashdefying@gmail.com>
2024-11-08 16:38:01 +0530

    yash1

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import './App.css'
+import { Signup } from './Components/Signup'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1 className='text-blue-700'>Frontend for Event Management App</h1>
-     <h2>Frontend for Event Management App</h2>
-     
+    <Signup />
     </>
   )
 }
---
+++ Frontend/src/Components/Signup.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Signup = () => {
+  return (
+    <>
+    <div className='h-[100vh] w-[100vw]  bg-[#2A182C] flex  justify-center items-center'>
+        <div className='h-[50vh] w-[50vw] bg-white flex'>
+            <div className='bg-red-300 flex-1'>hi</div>
+            <div className='bg-blue-300 flex-1'>Signup</div>
+        </div>
+    </div>
+    </>
+  )
+}
