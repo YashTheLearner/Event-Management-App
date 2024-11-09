aae34827d3173233af7b009e0097787c00109aec
Anand Patidar <patidaranand9752@gmail.com>
2024-11-09 19:46:19 +0530

    next commit anand comment controller

--- Backend/models/comment.model.js
+++ Backend/models/comment.model.js
@@ -2,27 +2,27 @@ import mongoose from "mongoose";
 import User from './app.model.js' // Import User model
 import Event from "./eventSchema.js" // Import Event model
 
+
 const commentSchema = new mongoose.Schema({
-  event: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Event', // Reference to the Event model
-    required: true
-  },
   text: {
     type: String,
-    required: true
+    required: true,
   },
-  user: {
+  createdBy: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User', // Reference to the User model
-    required: true
+    required: true,
+  },
+  eventId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Event', // Reference to the Event model
+    required: true,
   },
   createdAt: {
     type: Date,
-    default: Date.now()
-  }
-})
+    default: Date.now,
+  },
+});
 
-const Comment = mongoose.model('Comment', commentSchema)
-module.exports = Comment
-export default Comment
+const Comment = mongoose.model('Comment', commentSchema);
+export default Comment;
