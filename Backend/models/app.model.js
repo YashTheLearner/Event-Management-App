b48d4dda4e5de246bda977d373daf73cebfca0df
Yash Rajput <yashdefying@gmail.com>
2024-11-09 19:43:01 +0530

    added eventPreferences

--- Backend/models/app.model.js
+++ Backend/models/app.model.js
@@ -1,53 +1,63 @@
-//logic to create user collection and logic to define user schema
-
 import mongoose from "mongoose";
 
-const userschema =  new mongoose.Schema({
-    name : {
-        type: String,
-        required : true
+const userschema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    email : {
-        type : String,
-        minLength : 11,
-        lowercase : true,
-        required : true
+    email: {
+      type: String,
+      minLength: 11,
+      lowercase: true,
+      required: true,
     },
-    password : {
-        type : String,
-        required : true
+    password: {
+      type: String,
+      required: true,
     },
-    age:{
-        type: Number,
-        required : true
+    age: {
+      type: Number,
+      required: false,
     },
-    userBio:{
-        type: String,
-        required : false
+    userBio: {
+      type: String,
+      required: false,
     },
-    userLinkedIn:{
-        type: String,
-        required : false
+    userLinkedIn: {
+      type: String,
+      required: false,
     },
-    userInsta:{
-        type: String,
-        required : false
+    userInsta: {
+      type: String,
+      required: false,
     },
-    noOfCreated:{
-        type: Number,
-        default: 0,
-        required : false
+    noOfAttended: {
+      type: Number,
+      default: 0,
+      required: false,
     },
-    noOFHosted:{
-        type: Number,
-        default: 0,
-        required : false
+    noOfHosted: {
+      type: Number,
+      default: 0,
+      required: false,
     },
-    username:{
-        type: String,
-        required : true
-    }
-},{versionKey : false});
+    username: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: false,
+    },
+    eventPreferences: {
+      type: [String], // Array of strings
+      enum: ["Educational", "Music", "Cultural", "Spiritual", "Corporate"], // List of allowed values
+      required: false,
+    },
+  },
+  { versionKey: false }
+);
 
-const userModel =  mongoose.model("User",userschema);
-export default userModel
+const userModel = mongoose.model("User", userschema);
+export default userModel;
