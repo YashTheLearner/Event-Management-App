ff8ade1dfc2d77ed4d0da6253fca9254b18cc184
Yash Rajput <yashdefying@gmail.com>
2024-11-12 18:18:21 +0530

    YASH NAVBAR

--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -3,10 +3,21 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div className="home">
-      <h1>Welcome to the Home Page!</h1>
-      <p>You are now logged in.</p>
-    </div>
+    <>
+    <div className='h-[60px] bg-[#1b263b] py-[8px] px-[96px] flex justify-between'>
+      <div></div>
+      <div className='flex'>
+      <div className='text-[1.4rem] font-bold hover:text-[#ffcc00] text-[#EAEAEA]  tracking-wider'><i class="fas fa-home "></i> HOME</div>
+      <div className='text-[1.4rem] font-bold hover:text-[#ffcc00] text-[#EAEAEA] tracking-wider'><i class="fas fa-plus-circle"></i>CREATE EVENTS</div>
+      <input type="text"
+      placeholder='Search Event' />
+      <button class="bg-yellow-500 hover:bg-yellow-600 text-black font-semibold py-2 px-4 rounded" type="submit">Signup</button>
+      <button class="bg-yellow-500 hover:bg-yellow-600 text-black font-semibold py-2 px-4 rounded" type="submit">Login</button>
+     
+      
+      </div>
+      </div>
+    </>
   );
 };
 
